e8d94824855c2aa0ba65fa92fa8599c0cb4db9e9
Tobias Olsson <tobiasbjornolsson@gmail.com>
2017-02-21 11:43:33 +0100

    Radibutton selction mechanics added.

--- BreakingNewsTobiasOlsson/BreakingNewsTobiasOlsson/MainWindow.xaml.cs
+++ BreakingNewsTobiasOlsson/BreakingNewsTobiasOlsson/MainWindow.xaml.cs
@@ -29,5 +29,43 @@ namespace BreakingNewsTobiasOlsson
         {
             
         }
+
+        /// <summary>
+        /// This method haandles all radio button click events.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void rbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+           if(sender == rbtnTrmup)
+            { 
+                txtResult.Text = ("Trump");
+            }
+            else if (sender == rbtnEconomy)
+            {
+                txtResult.Text = ("Ekonomi");
+            }
+            else if (sender == rbtnCop)
+            {
+                txtResult.Text = ("polis");
+            }
+            else if (sender == rbtnSecret)
+            {
+                txtResult.Text = ("Helig");
+            }
+            else if (sender == rbtnAftonbladet)
+            {
+                txtResult.Text = ("Aftonbladet");
+            }
+            else if (sender == rbtnExpressen)
+            {
+                txtResult.Text = ("Expressen");
+            }
+            else if (sender == rbtnDagensNyheter)
+            {
+                txtResult.Text = ("Dagens nyheter");
+            }
+        }
     }
 }
---
+++ BreakingNewsTobiasOlsson/Interfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        public interface IWebCollector
+        {
+            /// <summary>
+            /// Html code from last search
+            /// </summary>
+            string HtmlCode { get; set; }
+            /// <summary>
+            /// Save html code from given URL to htmlCode property
+            /// If url is NULL, empty or not containing "http", save string.Empty
+            /// </summary>
+            /// <param name="url"></param>
+            /// <returns></returns>
+            void GetHTMLFromUrl(string url);
+        }
+
+        public interface IWebCalculator
+        {
+            /// <summary>
+            /// Calculates the number of occurences for given keyword in given html code.
+            /// Takes a IWebCollector as a parameter
+            /// Returns -1 if webcoll is NULL or Webcollector.Htmlcode property is
+            /// NULL/empty or if the keyword is NULL/empty
+            /// </summary>
+            int CalculateNumberOfHits(IWebCollector webColl, string keyword);
+        }
+    }
+}
